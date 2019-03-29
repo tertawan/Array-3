e24d9ca4587c0a90f0aa08e043f1a17bcfc126e7
Ninx <xasnaxxasnax@gmail.com>
2019-03-29 20:54:13 +0700

    Input MutiArray(3)

--- iostreamtest.cpp
+++ iostreamtest.cpp
@@ -1,46 +1,22 @@
-
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int main(){
+    int num[3][4][2];
+    char fac[5][20];
     
-    char month[30];
-    float max,min,avg;
-    int i = 0;
-    float num1,num2,num3,num4;
-    
-    FILE *pt;
-    
-    pt = fopen("/Users/tawannedsuwan/desktop/NewKK.txt","w");
-    
-    if(pt != NULL )
-    {
-        printf("Open file success!!\n\n");
-        
-        do{
-            printf("======================================\n\n");
-            printf("Enter month : ");
-            scanf("%s",month);
-            
-            if(strcmp(month,"Exit")== 0 )
-                break;//Exit
-            
-            printf("Enter north : ");
-            scanf("%f",&num1);
-            printf("Enter central : ");
-            scanf("%f",&num2);
-            printf("Enter South : ");
-            scanf("%f",&num3);
-            printf("Enter Esad : ");
-            scanf("%f",&num4);
-            
-            i = i + 1;
-            
-            fprintf(pt,"\n%d : %s  %.2f  %.2f  %.2f  %.2f",i,month,num1,num2,num3,num4);
-            
-        }while(strcmp(month,"Exit")!= 0);
-        
-        fclose(pt);
-    }else{
-        printf("Cannot open file !!!");
+    for(int i = 0; i < 5 ;i++){
+            printf("Faculty : ");
+            scanf("%s",&fac[i]);
+            if (strcmp(fac[i],"Exit") == 0 || strcmp(fac[i], "exit") == 0) {
+                break;
+            }
+        for (int j = 0; j < 4; j++) {
+            printf("Grade %d\n",j+1);
+            for (int k = 0; k < 3; k++) {
+                printf("Course %d Quantity Of Student : ",k+1);
+                scanf("%d",&num[i][j][k]);
+            }
+        }
     }
+    
 }
